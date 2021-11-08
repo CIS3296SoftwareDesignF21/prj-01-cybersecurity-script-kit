65ef714b4c3610d2f15d0a68274f4ef22fd8a958
Viraj Shah <shahvirajd@gmail.com>
2021-11-08 12:08:38 -0500

    Added caching to some functions to speed up runtime

--- src/getargs.py
+++ src/getargs.py
@@ -1,11 +1,17 @@
+from functools import lru_cache
+
+
+@lru_cache(maxsize=5)
 def removecmd(s: str) -> str:
     return s[s.index(" ") if " " in s else len(s) :]
 
 
+@lru_cache(maxsize=5)
 def getcmd(s: str) -> str:
     return s[: s.index(" ") if " " in s else len(s)]
 
 
+@lru_cache(maxsize=5)
 def getargs(s: str) -> list:
     args = []
     while s != "":
