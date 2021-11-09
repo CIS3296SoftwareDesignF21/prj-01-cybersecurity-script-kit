a20df8bcd7bf38cc7141435be476c6649b99bd97
Viraj Shah <shahvirajd@gmail.com>
2021-11-08 22:04:15 -0500

    Simplified getargs for now

--- src/getargs.py
+++ src/getargs.py
@@ -13,12 +13,4 @@ def getcmd(s: str) -> str:
 
 @lru_cache(maxsize=5)
 def getargs(s: str) -> list:
-    args = []
-    while s != "":
-        if s[0] == '"':
-            args.append(s[1 : s.index('"', 1)])
-            s = s[s.index('"', 1) + 1 :]
-        else:
-            args.append(s[: s.index(" ") if " " in s else len(s)])
-            s = s[(s.index(" ") if " " in s else len(s) - 1) + 1 :]
-    return args
+    return s.split(" ")
