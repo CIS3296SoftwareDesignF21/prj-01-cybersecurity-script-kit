fc094acd2685d842c573e0ee74bdd840ad036bb6
Viraj Shah <shahvirajd@gmail.com>
2021-11-15 12:52:33 -0500

    Added flagged commands to `csk` process

--- src/csk.py
+++ src/csk.py
@@ -1,5 +1,14 @@
 from sys import argv, exit
 from src import repl
+import platform
 
 if __name__ == "__main__":
-    exit(repl.main())
+    if len(argv) == 1:
+        exit(repl.main())
+    else:
+        if argv[1] == "-v" or argv[1] == "--version":
+            print(f"CSK v0.2.0 – {platform.system()}")
+        elif argv[1] == "-h" or argv[1] == "--help":
+            print("Usage: csk [OPTION]")
+        elif argv[1] == "-r" or argv[1] == "--run":
+            exit(0)
