be162803ace82db18a4397a1929bc37f23d1767d
Viraj Shah <shahvirajd@gmail.com>
2021-11-16 08:40:03 -0500

    Modified two scripts for compatibility with shell

--- src/kit/password_cracker.py
+++ src/kit/password_cracker.py
@@ -3,22 +3,23 @@
 import random
 import string
 
-#charters to use to guess against
-passChar = string.printable
-CharList = list(passChar)
-print(passChar)
+def run() -> None:
+    #charters to use to guess against
+    passChar = string.printable
+    CharList = list(passChar)
+    print(passChar)
 
-#Input to test if code is correct. 
-enteredPW = input("Test Password: ")
-#PWlength = len(enteredPW)
+    #Input to test if code is correct. 
+    enteredPW = input("Test Password: ")
+    #PWlength = len(enteredPW)
 
-guess = ""
+    guess = ""
 
-while(guess != enteredPW):
-    guess = random.choices(CharList, k=len(enteredPW))
-    print(str(guess))
+    while(guess != enteredPW):
+        guess = random.choices(CharList, k=len(enteredPW))
+        print(str(guess))
 
-    if(guess == enteredPW):
-        print("Password found: "+ "".join(guess))
-        break
+        if(guess == enteredPW):
+            print("Password found: "+ "".join(guess))
+            break
     
