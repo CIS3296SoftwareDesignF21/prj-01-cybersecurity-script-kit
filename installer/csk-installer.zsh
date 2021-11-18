fbe06bc509188d5e4862b6d99428c1bd39ab42a3
Viraj Shah <shahvirajd@gmail.com>
2021-11-18 14:19:15 -0500

    Updated install instructions to install latest version

--- installer/csk-installer.zsh
+++ installer/csk-installer.zsh
@@ -1,5 +1,5 @@
 wget https://github.com/CIS3296SoftwareDesignF21/prj-01-cybersecurity-script-kit/archive/refs/tags/v0.0.3-beta.zip
-unzip v0.0.2-beta.zip
+unzip v0.0.3-beta.zip
 cd prj-01-cybersecurity-script-kit-0.0.3-beta
 make clean
 make
